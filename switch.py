838307a46ebaaf227e44576bae75010402dd546e
YooJaemin <133607259+ingkoon8@users.noreply.github.com>
2023-05-17 12:51:52 +0900

    Update switch.py

--- switch.py
+++ switch.py
@@ -4,7 +4,7 @@ import time
 SW1 = 5
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(SW1,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(SW1,GPIO.IN,pull_up_down=GPIO.PUD_DOWN) # 풀다운 저항을 설정하여 floating상태 제거
 
 try:
     while True:
